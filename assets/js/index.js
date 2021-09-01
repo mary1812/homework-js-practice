cab572531a9400c2ebc924810625843bf011022f
Mary <mariyapiligrim3@gmail.com>
2021-09-02 01:31:15 +0300

    update all taska

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,12 +103,11 @@ for(let i = 0; i <= n; i++){
 if(i % 3 === 0 && i % 5 === 0 ) {
   console.log('fizzbuz')
 }
-else console.log(i)
+else
 if(i % 3 === 0) {
   console.log('fizz')
 }
-else console.log(i)
-
+else
 if(i % 5 === 0) {
   console.log('buzz')
 }
